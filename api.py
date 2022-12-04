53a4eaee64a482570fc249817785c87f49731f1f
Murillo Masson Guapo <97128702+murillomasson@users.noreply.github.com>
2022-12-04 01:54:08 -0300

    Update api.py

--- api.py
+++ api.py
@@ -17,32 +17,40 @@ db = client[DB]
 coll = db[COLL]
 
 
-@app.get("/weather/{city}", response_model=dict)
+@app.get("/{city}", response_model=dict)
 async def get_weather(city):
+    data = {}
     geolocation = json.loads(
         requests.get(
-                f"https://api.openweathermap.org/geo/1.0/direct?q={city}"
-                f"&limit=5&appid={API_KEY}&lang={lang}"
-            ).text
+            f"https://api.openweathermap.org/geo/1.0/direct?q="
+            f"{city}&limit=5&appid={API_KEY}&lang={lang}"
+        ).text
     )
-
-    lat = str(geolocation[0]['lat'])
-    lon = str(geolocation[0]['lon'])
-
     source = json.loads(
         requests.get(
-                f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}"
-                f"&appid={API_KEY}&lang={lang}&units=metric"
-            ).text
+            "https://api.openweathermap.org/data/2.5/forecast?"
+            "lat={}&lon={}&appid={}&lang={}&units=metric".format(
+                str(geolocation[0]['lat']),
+                str(geolocation[0]['lon']),
+                API_KEY,
+                lang,
+            )
+        ).text
     )
-
-    json_data = json.dumps(
-        source,
-        ensure_ascii=False
-    )
-
-    data = json.loads(json_data)
-    coll.insert_one(data)
     
-    return source
+    for i in source["list"]:
+        data[i["dt_txt"], geolocation[0]["name"]] = [i]
+    
+    coll.insert_one(
+        json.loads(
+            json.dumps(
+                {
+                    "city": geolocation[0]["name"],
+                    "timestamp": source["list"][0]["dt_txt"],
+                    "data": data
+                }
+            )
+        )
+    )
 
+    return data
