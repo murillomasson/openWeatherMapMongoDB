39a87aabdbee7f2f55dc7d3feb344efc597f85a7
Murillo Masson Guapo <97128702+murillomasson@users.noreply.github.com>
2022-12-04 03:29:24 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,25 +1,27 @@
-from fastapi import FastAPI
-import json
-import requests
 from pymongo import MongoClient
+from fastapi import FastAPI
+from datetime import datetime
+import requests
+import json
 
-# preencha as informações referentes à URI do MongoDB e à chave da API
-
-MONGO_CLIENT_URI = ""
+# preencha as informações referentes à URI do MongoDB e a chave da API
 API_KEY = ""
+lang = 'pt_br'
+MONGO_CLIENT_URI = ""
 DB = "weather"
 COLL = "cities"
-lang = 'pt_br'
 
-app = FastAPI()
 client = MongoClient(MONGO_CLIENT_URI)
 db = client[DB]
 coll = db[COLL]
 
+app = FastAPI()
+
 
 @app.get("/{city}", response_model=dict)
-async def get_weather(city):
+async def get_city(city):
     data = {}
+    now = datetime.now()
     geolocation = json.loads(
         requests.get(
             f"https://api.openweathermap.org/geo/1.0/direct?q="
@@ -37,20 +39,19 @@ async def get_weather(city):
             )
         ).text
     )
-    
+
     for i in source["list"]:
         data[i["dt_txt"]] = [i]
-    
+
     coll.insert_one(
         json.loads(
             json.dumps(
                 {
                     "city": geolocation[0]["name"],
-                    "timestamp": source["list"][0]["dt_txt"],
+                    "date_time": now.strftime("%m/%d/%Y, %H:%M:%S"),
                     "data": data
                 }
             )
         )
     )
-
-    return {"city": geolocation[0]["name"], "timestamp": source["list"][0]["dt_txt"], "data": data}
+    return {"city": geolocation[0]["name"], "date_time": now.strftime("%m/%d/%Y, %H:%M:%S"), "data": data}
